801f6ad4dc3e147514c841e994d9fe2cb37bcf12
AS042971 <as042971@outlook.com>
2023-07-25 22:56:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Reddit r/place 2023 Timelapse Generator for Tiles
+
+This application generates the HD Timelapse video for Reddit r/place 2023.
+
+
+## Usage
+
+1. Go to the `reference` folder and find `frame_id` with the 4 boundary positions of the target tile. For instance, the Genshin Impact tile is located in frame `4`, with `left=400`, `upper=240`, `right=530` and `lower=360`
+
+2. In this folder, run the command `python -m pip install -r requirements.txt` to install dependencies.
+
+3. run `python .\get_tiles.py [frame_id] [left] [upper] [right] [lower]` to download the clipped image sequence. For instance, to download the Genshin Impact tile, the command should be `python .\get_tiles.py 4 400 240 530 360`. The files will be stored in `.\output` folder, or you can pass a custom `--output` command.
+
+## Video generation
+
+To generate a video, you should first install `ffmpeg` and add it into `PATH`.
+
+The basic command is `ffmpeg -framerate 60 -i ./build/%05d.png output.mp4`, which will generate a timelapse video at 3600 timescale (1 second for 1 hour).
+You can change the timescale by modifying `framerate`
+
+To upscale the video, pass `-vf scale=iw*2:ih*2:flags=neighbor` to the command, where `2` is the scale ratio.
+For instance, `ffmpeg -framerate 60 -i ./build/%05d.png -vf scale=iw*10:ih*10:flags=neighbor output.mp4` generates a 1300x1200 Genshin Impact video
---
+++ gen_video.py
@@ -0,0 +1,47 @@
+import argparse
+import csv
+from io import BytesIO
+
+import requests
+from PIL import Image
+from alive_progress import alive_bar
+
+
+def clipImage(index: int, id: str, url: str, left: int, upper: int, right: int, lower: int, out: str):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    cropped = image.crop((left, upper, right, lower))
+    cropped.save(f'{index:05}/{id}.png')
+
+def genVideo(frame: str, left: int, upper: int, right: int, lower: int, out: str):
+    csv_path = f'./data/{frame}.csv'
+    with open(csv_path, newline='') as f:
+        row_count = sum(1 for line in f)
+    with open(csv_path, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        with alive_bar(row_count) as bar:
+            for index, row in enumerate(spamreader):
+                clipImage(index, row[0], 'https://garlic-bread.reddit.com/media/canvas-images/full-frame/' + row[1], left, upper, right, lower, out)
+                bar()
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("frame",
+                        help="The frame of the full r/place canvas. Possible values: 0 to 5.")
+    parser.add_argument("left",
+                        help="The left column index for the clipped image, use reference/{frame}.png to locate.")
+    parser.add_argument("upper",
+                        help="The upper row index for the clipped image, use reference/{frame}.png to locate.")
+    parser.add_argument("right",
+                        help="The right column index for the clipped image, use reference/{frame}.png to locate.")
+    parser.add_argument("lower",
+                        help="The lower row index for the clipped image, use reference/{frame}.png to locate.")
+    parser.add_argument("--out", default="./output",
+                        help="Output directory for the clipped images")
+    args = parser.parse_args()
+    genVideo(args.frame, int(args.left), int(args.upper), int(args.right), int(args.lower), args.out)
+
+
+if __name__ == '__main__':
+    main()
